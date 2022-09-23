6c65bd7f21b07178218f2e5c223d0fe041100229
Esneider M <esneidermanuelbuelvas1997@gmail.com>
2022-09-23 12:23:39 -0500

    api res productos lista

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,8 +3,47 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+// use Validator; // remplazada por "use Illuminate\Support\Facades\Validator;"
+use App\Models\User;
+use \stdClass;
+
+/* ## Clases llamadas por mi ##  */
+//use Illuminate\Validation\ValidationException;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
-    //
+    /**
+     * Configure the validator instance.
+     *
+     * @param  \Illuminate\Validation\Validator  $validator
+     * @return void
+     */
+    public function register(Request $request)
+    {
+        
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|max:8'
+        ]);
+        
+        if($validator->fails()){
+            return response()->json($validator->errors());
+        }
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password)
+        ]);
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return  response()
+                    ->json( [ 'data'=>$user,'access_token' => $token, 'token_type' => 'Bearer' ] );
+
+    }
 }
--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -15,6 +15,8 @@ class ProductController extends Controller
     public function index()
     {
         //
+        $product = Product::all();
+        return $product;
     }
 
     /**
